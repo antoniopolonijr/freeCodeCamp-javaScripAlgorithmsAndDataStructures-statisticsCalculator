2a6dd0d85dc19a555f6dff6758ce60eb3063905f
Antonio Poloni <antoniopolonijr@gmail.com>
2024-07-30 14:07:43 -0300

    Implement the function to get the mean

--- script.js
+++ script.js
@@ -7,4 +7,19 @@ const calculate = () => {
   // A user could put any text they want into the input box. You want to make sure that you are only working with numbers. The Number() constructor will return NaN (which stands for "not a number") if the value passed to it cannot be converted to a number. You need to filter these values out – thankfully, arrays have a method specifically for this. The .filter() method will allow you to filter elements out of an array, creating a new array in the process.
   // Much like the .map() method, the .filter() method takes a callback function. The callback function takes the current element as its first argument. The callback function needs to return a Boolean value, which indicates whether the element should be included in the new array. In this case, you want to return true if the element is not NaN (not a number). However, you cannot check for equality here, because NaN is not equal to itself. Instead, you can use the isNaN() method, which returns true if the argument is NaN.
   // Array methods can often be chained together to perform multiple operations at once.
+  const mean = getMean(numbers);
+  document.querySelector("#mean").textContent = mean; // To display the value of mean
+  // If you test your form with a list of numbers, you should see the mean display on the page. However, this only works because freeCodeCamp's iframe has special settings. Normally, when a form is submitted, the event triggers a page refresh. To resolve this, add return false; after your calculate(); call in the onsubmit attribute.
 };
+
+// function to get the mean, it is the average value of all numbers in a list.
+const getMean = (array) =>
+  array.reduce((acc, el) => acc + el, 0) / array.length;
+// const sum = array.reduce((acc, el) => acc + el, 0); // clean this logic up a bit. Using the implicit return of an arrow function
+// The first step in calculating the mean is to take the sum of all numbers in the list. Arrays have another method, called .reduce(), which is perfect for this situation. The .reduce() method takes an array and applies a callback function to condense the array into a single value.
+// Like the other methods, .reduce() takes a callback. This callback, however, takes at least two parameters. The first is the accumulator, and the second is the current element in the array. The return value for the callback becomes the value of the accumulator on the next iteration.
+// The .reduce() method takes a second argument that is used as the initial value of the accumulator. Without a second argument, the .reduce() method uses the first element of the array as the accumulator, which can lead to unexpected results. To be safe, it's best to set an initial value. Set the initial value of the accumulator to 0.
+// const mean = sum / array.length; // clean this logic up a bit. Using the implicit return of an arrow function
+// The next step in calculating the mean is to divide the sum of numbers by the count of numbers in the list.
+// return mean; // clean this logic up a bit. Using the implicit return of an arrow function
+// You can actually clean this logic up a bit. Using the implicit return of an arrow function, you can directly return the value of the .reduce() method divided by the length of the array, without having to assign any variables.
