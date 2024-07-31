096e7b2fc10222a2d3d5ba047694d1c7f55d622f
Antonio Poloni <antoniopolonijr@gmail.com>
2024-07-31 12:29:21 -0300

    Implement the function to get the median

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-// this function is called when the form is submitted <form onsubmit="calculate();">
-const calculate = () => {
-  const value = document.querySelector("#numbers").value; // to find the number that was entered in the #numbers input field
-  const array = value.split(/,\s*/g); // you need to split it into an array of numbers. The .split() method takes a string and splits it into an array of strings. You can pass it a string of characters or a RegEx to use as a separator. For example, string.split(",") would split the string at each comma and return an array of strings. Use the /,\s*/g regex to split the value string by commas. You can tweak it based on the number of spaces separating your values.
-  const numbers = array.map((el) => Number(el)).filter((el) => !isNaN(el)); // The value of an input element is always a string, even if the input type is number. You need to convert this array of strings into an array of numbers. To do this, you can use the .map() method. Remember that .map() creates a new array, instead of mutating the original array. // The .map() method takes a callback function as its first argument. This callback function takes a few arguments, but the first one is the current element being processed. The callback function needs to return a value. In this case, you want to return the value of each element converted to a number. You can do this by using the Number() constructor, passing the element as an argument.
-  // const filtered = numbers.filter((el) => !isNaN(el)); // chained together to perform multiple operations at once.
-  // A user could put any text they want into the input box. You want to make sure that you are only working with numbers. The Number() constructor will return NaN (which stands for "not a number") if the value passed to it cannot be converted to a number. You need to filter these values out – thankfully, arrays have a method specifically for this. The .filter() method will allow you to filter elements out of an array, creating a new array in the process.
-  // Much like the .map() method, the .filter() method takes a callback function. The callback function takes the current element as its first argument. The callback function needs to return a Boolean value, which indicates whether the element should be included in the new array. In this case, you want to return true if the element is not NaN (not a number). However, you cannot check for equality here, because NaN is not equal to itself. Instead, you can use the isNaN() method, which returns true if the argument is NaN.
-  // Array methods can often be chained together to perform multiple operations at once.
-  const mean = getMean(numbers);
-  document.querySelector("#mean").textContent = mean; // To display the value of mean
-  // If you test your form with a list of numbers, you should see the mean display on the page. However, this only works because freeCodeCamp's iframe has special settings. Normally, when a form is submitted, the event triggers a page refresh. To resolve this, add return false; after your calculate(); call in the onsubmit attribute.
-};
-
 // function to get the mean, it is the average value of all numbers in a list.
 const getMean = (array) =>
   array.reduce((acc, el) => acc + el, 0) / array.length;
@@ -23,3 +9,77 @@ const getMean = (array) =>
 // The next step in calculating the mean is to divide the sum of numbers by the count of numbers in the list.
 // return mean; // clean this logic up a bit. Using the implicit return of an arrow function
 // You can actually clean this logic up a bit. Using the implicit return of an arrow function, you can directly return the value of the .reduce() method divided by the length of the array, without having to assign any variables.
+
+// function to get the median calculation. The median is the midpoint of a set of numbers.
+const getMedian = (array) => {
+  const sorted = array.sort((a, b) => a - b); // The first step in calculating the median is to ensure the list of numbers is sorted from least to greatest. Once again, there is an array method ideal for this – the .sort() method.
+  // By default, the .sort() method converts the elements of an array into strings, then sorts them alphabetically. This works well for strings, but not so well for numbers. For example, 10 comes before 2 when sorted as strings, but 2 comes before 10 when sorted as numbers. To fix this, you can pass in a callback function to the .sort() method. This function takes two arguments, which represent the two elements being compared. The function should return a value less than 0 if the first element should come before the second element, a value greater than 0 if the first element should come after the second element, and 0 if the two elements should remain in their current positions. To sort your numbers from smallest to largest, pass a callback function that takes parameters a and b, and returns the result of subtracting b from a.
+  const median =
+    array.length % 2 === 0 // check if the length of array is even.
+      ? getMean([sorted[array.length / 2], sorted[array.length / 2 - 1]]) // If it even, find the middle two numbers, calculate their mean
+      : sorted[Math.floor(array.length / 2)]; // If it is odd, return the middle number.
+  return median;
+  /*
+In the next few steps, you'll learn how to determine if an array's length is even or odd, as well as how to find the median. You will then be able to apply what you learned to the getMedian function.
+To check if a number is even or odd, you can use the modulus operator %. The modulus operator returns the remainder of the division of two numbers.
+Here is an example checking if an array length is even or odd:
+Example Code
+// check if array length is even
+arr.length % 2 === 0;
+// check if array length is odd
+arr.length % 2 === 1;
+If the remainder is 0, the number is even. If the remainder is 1, the number is odd.
+const testArr1 = [1, 2, 3, 4, 5];
+const testArr2 = [1, 2, 3, 4, 5, 6];
+const isEven = testArr2.length % 2 === 0;
+console.log(isEven);
+To get the median of an array with an odd number of elements, you will need to find and return the middle number.
+Here is how to find the middle number of an array with an odd number of elements:
+Example Code
+arr[Math.floor(arr.length / 2)];
+Here is a longer example finding the middle number of an array with 5 elements:
+Example Code
+const numbers = [1, 2, 3, 4, 5];
+const middleNumber = numbers[Math.floor(numbers.length / 2)];
+console.log(middleNumber); // 3
+The reason why you use Math.floor is because the result of dividing an odd number by 2 will be a decimal. Math.floor will round down to the nearest whole number.
+const oddListMedian = testArr1[Math.floor(testArr1.length / 2)];
+console.log(oddListMedian);
+To find the median of an even list of numbers, you need to find the two middle numbers and calculate the mean of those numbers.
+Here is how to find the two middle numbers of an even list of items:
+Example Code
+// first middle number
+arr[arr.length / 2];
+// second middle number
+arr[(arr.length / 2) - 1];
+To find the median, you can use the getMean function which adds the middle numbers and divides the sum by 2.
+Example Code
+const numbers = [1, 2, 3, 4];
+const firstMiddleNumber = numbers[numbers.length / 2];
+const secondMiddleNumber = numbers[(numbers.length / 2) - 1];
+// result is 2.5
+getMean([firstMiddleNumber, secondMiddleNumber]);
+const evenListMedian = getMean([
+  testArr2[testArr2.length / 2 - 1],
+  testArr2[testArr2.length / 2],
+]);
+console.log(evenListMedian);
+Now that you have a better understanding of how to find the median for odd and even lists of numbers, you can remove all your test code from the previous steps.
+*/
+};
+
+// this function is called when the form is submitted <form onsubmit="calculate();">
+const calculate = () => {
+  const value = document.querySelector("#numbers").value; // to find the number that was entered in the #numbers input field
+  const array = value.split(/,\s*/g); // you need to split it into an array of numbers. The .split() method takes a string and splits it into an array of strings. You can pass it a string of characters or a RegEx to use as a separator. For example, string.split(",") would split the string at each comma and return an array of strings. Use the /,\s*/g regex to split the value string by commas. You can tweak it based on the number of spaces separating your values.
+  const numbers = array.map((el) => Number(el)).filter((el) => !isNaN(el)); // The value of an input element is always a string, even if the input type is number. You need to convert this array of strings into an array of numbers. To do this, you can use the .map() method. Remember that .map() creates a new array, instead of mutating the original array. // The .map() method takes a callback function as its first argument. This callback function takes a few arguments, but the first one is the current element being processed. The callback function needs to return a value. In this case, you want to return the value of each element converted to a number. You can do this by using the Number() constructor, passing the element as an argument.
+  // const filtered = numbers.filter((el) => !isNaN(el)); // chained together to perform multiple operations at once.
+  // A user could put any text they want into the input box. You want to make sure that you are only working with numbers. The Number() constructor will return NaN (which stands for "not a number") if the value passed to it cannot be converted to a number. You need to filter these values out – thankfully, arrays have a method specifically for this. The .filter() method will allow you to filter elements out of an array, creating a new array in the process.
+  // Much like the .map() method, the .filter() method takes a callback function. The callback function takes the current element as its first argument. The callback function needs to return a Boolean value, which indicates whether the element should be included in the new array. In this case, you want to return true if the element is not NaN (not a number). However, you cannot check for equality here, because NaN is not equal to itself. Instead, you can use the isNaN() method, which returns true if the argument is NaN.
+  // Array methods can often be chained together to perform multiple operations at once.
+  const mean = getMean(numbers);
+  const median = getMedian(numbers);
+  document.querySelector("#mean").textContent = mean; // To display the value of mean
+  // If you test your form with a list of numbers, you should see the mean display on the page. However, this only works because freeCodeCamp's iframe has special settings. Normally, when a form is submitted, the event triggers a page refresh. To resolve this, add return false; after your calculate(); call in the onsubmit attribute.
+  document.querySelector("#median").textContent = median;
+};
